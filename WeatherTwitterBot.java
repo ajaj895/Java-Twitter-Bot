135514aa719ad897526b4dc737edfa07fb12731b
Evan <41348743+ajaj895@users.noreply.github.com>
2019-03-08 12:24:48 -0600

    Updated TwitterBot and Twitter helper class
    Reworking the main twitterbot class, so it currently doesn't do anything for now. The helper class has many new methods to make my life easier and helpful. 
    -removed Herobrine

--- WeatherTwitterBot.java
+++ WeatherTwitterBot.java
@@ -5,35 +5,39 @@
  */
 package weathertwitterbot;
 
-import java.io.PrintStream;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
+//import java.io.PrintStream;
+//import twitter4j.Status;
+//import twitter4j.Twitter;
+//import twitter4j.TwitterFactory;
 
-import java.io.IOException;//for connection issues
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;//for connection issues
+import org.jsoup.nodes.Element;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.TwitterException;
+
 /**
  *
  * @author Evan
  */
-public class WeatherTwitterBot {
-    //private static PrintStream consolePrint;
+public class WeatherTwitterBot{//TODO: Javadocs and major comment cleanup like seriously, this is a MESS
     
-    private static PrintStream consolePrint;
-    
+    private static PrintStream consolePrint;//a printstream prints all of what java does. Idk much else though
+    private static boolean autoContinue = true;//a boolean for continuing automatic posting.
+
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws TwitterException,IOException {
-        
+    public static void main(String[] args) throws TwitterException,IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        int runTime = 24;//in hours
         Twitterer tweety = new Twitterer(consolePrint);
+
         
-        String message= "Hello Twitter! This is a test of the java twitter4j API.";
-        tweety.tweetOut(message);
-
-        // TODO code application logic here
     }
-
-    
 }
