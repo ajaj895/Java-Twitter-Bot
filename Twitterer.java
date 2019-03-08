1dd0e69ee2b164c63c2256708a925e89f6ea0015
Evan <41348743+ajaj895@users.noreply.github.com>
2019-03-08 10:47:26 -0600

    Updated Helper class
    Now with more features
    -removed Herobrine

--- Twitterer.java
+++ Twitterer.java
@@ -7,14 +7,16 @@ package weathertwitterbot;
 
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.Status;
 import twitter4j.TwitterException;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import twitter4j.DirectMessage;
+import twitter4j.DirectMessageList;
 import twitter4j.ResponseList;
 
 /**
@@ -30,7 +32,7 @@ public class Twitterer {//TODO: J A V A D O C S!
         //makes a twitter object and connects and does that login stuff
         twitter = TwitterFactory.getSingleton();
         consolePrint = console;
-        statuses = new ArrayList<Status>();
+        
     }
     //tweets a given message
     /**
@@ -58,7 +60,12 @@ public class Twitterer {//TODO: J A V A D O C S!
     public ResponseList<Status> getRecents() throws TwitterException{
         return twitter.getUserTimeline();
     }
-    
+    public ResponseList<Status> getMentions() throws TwitterException{
+        return twitter.getMentionsTimeline();
+    }
+    public ResponseList<Status> getRetweets() throws TwitterException{//get retweets of me. I might add a method to get a specific post's retweets later.
+        return twitter.getRetweetsOfMe();
+    }
     private long getPostAge(Status status){//returns age of post in miliseconds (starting date Jan 1 1970
         long age = status.getCreatedAt().getTime();
         return age;
@@ -79,8 +86,47 @@ public class Twitterer {//TODO: J A V A D O C S!
         long age = System.currentTimeMillis()-getPostAge(status);
         return TimeUnit.MILLISECONDS.toSeconds(age);
     }
+    private long myId() throws TwitterException{
+        return twitter.getId();
+    }
     public boolean dayOld(Status status){
+        
         return (ageToDays(status)>0);
     }
+    public boolean keywordCheck(String word, Status status){
+        Scanner sc = new Scanner(status.getText());
+        boolean key = false;
+        while(sc.hasNext()){
+            if(sc.next().equalsIgnoreCase(word)){
+                key = true;
+                return key;
+            }
+        } 
+        return key;
+    }
+    //might return null if DirectMessageList is empty.
+    public DirectMessageList getAllMessages(int howMany) throws TwitterException{
+        if(howMany > 50){
+            howMany = 50;
+            System.err.println("Too many messages to return, returning the maximum of 50.");
+        }else if(howMany < 0){
+            howMany = 1;
+            System.err.println("Message number can't be less than zero. Returning 1 message.");
+        }
+        return twitter.getDirectMessages(howMany);
+    }
+    public void getReceivedMessages(DirectMessageList allMessages, LinkedList<DirectMessage> sentMessages, LinkedList<DirectMessage> receivedMessages) throws TwitterException{
+        for(int i = 0; i<allMessages.size();){
+            if(allMessages.get(i).getRecipientId() == myId()){
+                receivedMessages.addLast(allMessages.remove(i));
+            }else{
+                sentMessages.addLast(allMessages.remove(i));
+            }
+        }
+        
+    }
+
+    
+    
     
 }
